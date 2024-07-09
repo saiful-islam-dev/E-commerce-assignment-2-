4fc82bb7b4665cdc9729f090132218ea2ff0b144
Saiful264 <saifulislamx264@gmail.com>
2024-07-09 23:31:57 +0600

    Diploy sucessfully on vercell

--- dist/app/router/index.js
+++ dist/app/router/index.js
@@ -2,11 +2,16 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const product_router_1 = require("../modules/Product/product.router");
+const order_router_1 = require("../modules/Order/order.router");
 const router = (0, express_1.Router)();
 const moduleRoutes = [
     {
         path: '/api',
         route: product_router_1.ProductRoutes,
+    },
+    {
+        path: '/api',
+        route: order_router_1.OrderRoutes,
     },
 ];
 moduleRoutes.forEach((route) => {
